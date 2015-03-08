f3321cb172fe030a911c06b0164150e4fb676bc3
beeven <beeven@hotmail.com>
2015-03-08 22:49:04 +0800

    Fixed double free pointer. Tested against 0.10.36, 0.12.0, 1.4.1

--- b32.cc
+++ b32.cc
@@ -8,7 +8,7 @@ using namespace node;
 
 const char *b32table = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
 
-int base32_encode(const char *data, size_t length, char *result, size_t bufSize) {
+int base32_encode(const char *data, size_t length, char *result, size_t buf_size) {
 	if(length < 0 || length > (1 << 28)) {
 		return -1;
 	}
@@ -16,35 +16,35 @@ int base32_encode(const char *data, size_t length, char *result, size_t bufSize)
 	if(length > 0) {
 		int buffer = data[0];
 		int next = 1;
-		int bitsLeft = 8;
-		while(count < bufSize && (bitsLeft > 0 || next < length)) {
-			if(bitsLeft < 5) {
+		int bits_left = 8;
+		while(count < buf_size && (bits_left > 0 || next < length)) {
+			if(bits_left < 5) {
 				if(next < length) {
 					buffer <<= 8;
 					buffer |= data[next++] & 0xff;
-					bitsLeft += 8;
+					bits_left += 8;
 				} else {
-					int pad = 5 - bitsLeft;
+					int pad = 5 - bits_left;
 					buffer <<= pad;
-					bitsLeft += pad;
+					bits_left += pad;
 				}
 			}
-			int index = 0x1f & (buffer >> (bitsLeft - 5));
-			bitsLeft -= 5;
+			int index = 0x1f & (buffer >> (bits_left - 5));
+			bits_left -= 5;
 			result[count++] = b32table[index];
 		}
 	}
-	if(count < bufSize) {
+	if(count < buf_size) {
 		result[count] = '\000';
 	}
 	return count;
 }
 
-int base32_decode(const char *encoded, char *result, size_t bufSize) {
+int base32_decode(const char *encoded, char *result, size_t buf_size) {
 	int buffer = 0;
-	int bitsLeft = 0;
+	int bits_left = 0;
 	int count = 0;
-	for(const char *ptr = encoded; count < bufSize && *ptr && *ptr!='='; ++ptr) {
+	for(const char *ptr = encoded; count < buf_size && *ptr && *ptr!='='; ++ptr) {
 		char ch = *ptr;
 		if(ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n' || ch == '-') {
 			continue;
@@ -69,13 +69,13 @@ int base32_decode(const char *encoded, char *result, size_t bufSize) {
 		}
 
 		buffer |= ch;
-		bitsLeft += 5;
-		if(bitsLeft >= 8) {
-			result[count++] = buffer >> (bitsLeft - 8);
-			bitsLeft -= 8;
+		bits_left += 5;
+		if(bits_left >= 8) {
+			result[count++] = buffer >> (bits_left - 8);
+			bits_left -= 8;
 		}
 	}
-	if(count < bufSize) {
+	if(count < buf_size) {
 		result[count] = '\000';
 	}
 	return count;
@@ -87,34 +87,34 @@ public:
 	const char* input;
 	const size_t input_size;
 	char* result;
-	size_t result_size;
+	int result_size;
 
 	B32Async(NanCallback *callback, int method,const char* input, const size_t input_size):NanAsyncWorker(callback),
 		method(method),input(input), input_size(input_size) {
 	}
 
 	~B32Async() {
-		delete result;
+		//delete result;
 	}
 
 	void Execute() {
-		size_t bufSize;
+		size_t buf_size;
 		if(method == 1) { // encode
-			bufSize  = (input_size * 8 - 1)/5+1;
+			buf_size  = (input_size * 8 - 1)/5+1;
 			result = new char[input_size];
-			if((result_size=base32_encode(input,input_size,result,bufSize)) == -1) {
+			if((result_size=base32_encode(input,input_size,result,buf_size)) == -1) {
 				SetErrorMessage("encode error");
 			}
 			
 		} else { // decode
 			size_t size = strlen(input);
-			bufSize = (size * 5 )/8;
-			result = new char[bufSize];
-			if((result_size = base32_decode(input,result,bufSize)) == -1) {
+			buf_size = (size * 5 )/8;
+			result = new char[buf_size];
+			if((result_size = base32_decode(input,result,buf_size)) == -1) {
 				SetErrorMessage("decode error");
 			}
 		}
-		//result_size = bufSize;
+		//result_size = buf_size;
 	}
 
 	void HandleOKCallback() {
@@ -142,10 +142,10 @@ NAN_METHOD(encodeSync) {
 	size_t size = Buffer::Length(arg->ToObject());
 	char* buf = Buffer::Data(arg->ToObject());
 
-	size_t bufSize = (size * 8 - 1)/5+1;
-	char *result = new char[bufSize];
+	size_t buf_size = (size * 8 - 1)/5+1;
+	char *result = new char[buf_size];
 	int result_size = 0;
-	if((result_size = base32_encode(buf,size,result,bufSize))< 0) {
+	if((result_size = base32_encode(buf,size,result,buf_size))< 0) {
 		NanThrowError("encode error");
 		NanReturnUndefined();
 	} else {
@@ -169,10 +169,10 @@ NAN_METHOD(decodeSync) {
 	size_t size = Buffer::Length(arg->ToObject());
 	char* buf = Buffer::Data(arg->ToObject());
 
-	size_t bufSize = (size * 5 )/8;
-	char *result = new char[bufSize];
+	size_t buf_size = (size * 5 )/8;
+	char *result = new char[buf_size];
 	int result_size = 0;
-	if((result_size = base32_decode(buf,result,bufSize)) < 0) {
+	if((result_size = base32_decode(buf,result,buf_size)) < 0) {
 		NanThrowError("decode error");
 		NanReturnUndefined();
 	} else {
